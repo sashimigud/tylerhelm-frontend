e8780a5824fec1cb90c03144997ee3c93c11b7c6
trym <trym_helle@hotmail.com>
2021-06-26 01:51:48 +0200

    removing some deps vulnerabilities, adding about-shell

---
+++ src/components/about/About.component.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './about.styles.scss';
+
+const About: FC = () => {
+  return (
+    <div className="about-container">
+      
+    </div>
+  )
+} 
+
+export default About;
