a710003b4424ac664d8da7446029d7d5cde94302
trym <trym_helle@hotmail.com>
2021-06-26 01:30:56 +0200

    adding semi-random gifs around conntainer

--- src/components/home/content/Content.component.tsx
+++ src/components/home/content/Content.component.tsx
@@ -3,9 +3,22 @@ import './content.styles.scss'
 
 const Content: FC = () => {
   return (<div className="content-container">
-    <div className="content-left"></div>
-    <div className="content-middle"></div>
-    <div className="content-right"></div>
+    <div className="content-left">
+      <img className="left-img l-img-1" src="assets/cat.gif" alt="" />
+      <img className="left-img l-img-2" src="assets/lame.gif" alt="" />
+      <img className="left-img l-img-3" src="assets/pyramide.gif" alt="" />
+      <img className="left-img l-img-4" src="assets/heart.gif" alt="" />
+      <img className="left-img l-img-5" src="assets/smiley.gif" alt="" />
+    </div>
+    <div className="content-middle">
+      
+    </div>
+    <div className="content-right">
+      <img className="right-img r-img-1" src="assets/sunglasses.gif" alt="" />
+      <img className="right-img r-img-2" src="assets/lips.png" alt="" />
+      <img className="right-img r-img-3" src="assets/ksigns.gif" alt="" />
+      <img className="right-img r-img-4" src="assets/cherry.png" alt="" />
+    </div>
   </div>)
 }
 
