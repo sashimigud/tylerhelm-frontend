6cf153c1292f6812785b716190a26dbd07901b36
trym <trym_helle@hotmail.com>
2021-06-28 23:25:58 +0200

    adding carousel start

--- src/components/common/carousel/Carousel.component.tsx
+++ src/components/common/carousel/Carousel.component.tsx
@@ -1,12 +1,32 @@
-import React, { FC } from 'react';
-import './carousel.styles.scss';
+import React, { FC } from "react";
+import "./carousel.styles.scss";
+import { useSpringCarousel } from "react-spring-carousel-js";
 
 const Carousel: FC = () => {
+  const { carouselFragment, slideToPrevItem, slideToNextItem } =
+    useSpringCarousel({
+      withThumbs: true,
+      items: [
+        {
+          id: "item-1",
+          renderItem: <div>Item 1</div>,
+        },
+        {
+          id: "item-2",
+          renderItem: <div>Item 2</div>,
+        },
+      ],
+    });
+
   return (
     <div className="carousel-container">
-
+      {/*
+    <p>tittel</p>
+    mulig at renderItem kan ha tittel også
+    */}
+      <div className="carousel-wrapper">{carouselFragment}</div>
     </div>
-  )
-}
+  );
+};
 
 export default Carousel;
