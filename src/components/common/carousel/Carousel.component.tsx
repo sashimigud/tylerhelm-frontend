49100dc408bbc3a3174c0d386dcaa0ebfda2da1c
trym <trym_helle@hotmail.com>
2021-06-29 21:42:39 +0200

    adding carousel start

--- src/components/common/carousel/Carousel.component.tsx
+++ src/components/common/carousel/Carousel.component.tsx
@@ -3,17 +3,26 @@ import "./carousel.styles.scss";
 import { useSpringCarousel } from "react-spring-carousel-js";
 
 const Carousel: FC = () => {
-  const { carouselFragment, slideToPrevItem, slideToNextItem } =
+  const { carouselFragment, thumbsFragment, slideToPrevItem, slideToNextItem, slideToItem } =
     useSpringCarousel({
+      //index of item to start with
+      initialActiveItem: 1,
       withThumbs: true,
       items: [
         {
           id: "item-1",
           renderItem: <div>Item 1</div>,
+          //slideToItem takes index and id
+          renderThumb: (<div onClick={() => slideToItem("item-1")}>
+            thumb
+            </div>)
         },
         {
           id: "item-2",
           renderItem: <div>Item 2</div>,
+          renderThumb: (<div>
+            thumb
+          </div>)
         },
       ],
     });
@@ -24,7 +33,14 @@ const Carousel: FC = () => {
     <p>tittel</p>
     mulig at renderItem kan ha tittel også
     */}
-      <div className="carousel-wrapper">{carouselFragment}</div>
+      <div className="carousel-wrapper">
+        <button onClick={slideToNextItem}>next</button>
+        {carouselFragment}
+        <button onClick={slideToPrevItem}>back</button>
+      </div>
+      <div className="thumbs-wrapper">
+        {thumbsFragment}
+      </div>
     </div>
   );
 };
