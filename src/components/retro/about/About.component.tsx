e1a4d773f1a92d46e02c75dfa75637ef990524f8
trym <trym_helle@hotmail.com>
2021-10-04 00:54:55 +0200

    changing about text

--- src/components/retro/about/About.component.tsx
+++ src/components/retro/about/About.component.tsx
@@ -43,7 +43,15 @@ const About: FC = () => {
           <h1>Hello world.</h1>
           Welcome to my corner of the internet. <br />
           <br /> Consider this an homage to a time before every inch of the
-          internet was analysed and commercialized to hell. <br />
+          internet was analysed and commercialized to the extent seen today.
+          <br />
+          <br />
+          Forget likes or brand deals, people made shit because they enjoyed it.
+          Did most of it suck? Absolutely. is it stupid to have a 20mb
+          bundle-size of unnecessary gifs? you bet. Sometimes you just want to
+          say to hell with SEO, fuck accessibility, optimization? - who needs
+          it? Look at this stupid shit I made! <br /> - and that's ok.
+          <br />
           <br /> If you should want the more sanitized and normal experience
           there is a retro-switch in the right-hand corner, press it and you'll
           be sent to the boring version of this site. <br />
