ed0bfff7c95f17895836ddd8dba07319e9fd146c
trym <trym_helle@hotmail.com>
2021-10-03 03:21:29 +0200

    adding chains

--- src/components/retro/nfts/Nfts.component.tsx
+++ src/components/retro/nfts/Nfts.component.tsx
@@ -2,7 +2,16 @@ import React, { FC } from 'react';
 import './nfts.styles.scss';
 
 const Nfts: FC = () => {
-  return <div className="nfts-container">NFTs</div>;
+  return (
+    <div className="nfts-container">
+      <h2>Chains:</h2>
+      <h3>Etherium</h3>
+      <h3>Algorand</h3>
+      <h3>Polygon/Matic</h3>
+      <h3>Tezos</h3>
+      <h3>Harmoney One</h3>
+    </div>
+  );
 };
 
 export default Nfts;
